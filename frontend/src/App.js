d4a654467a9118e73b162d20bc46c2efcb9d55e5
Stanislava Pavlova <stanislavap2002@gmail.com>
2024-03-23 19:08:48 +0200

    Format files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/home';
 
 function App() {
-	return (
-		<ChakraProvider>
-			<Container maxW="1200px" py="10" centerContent>
-				<Router>
-					<Routes>
-						<Route index element={<Homepage />} />
-					</Routes>
-				</Router>
-			</Container>
-		</ChakraProvider>
-	);
+    return (
+        <ChakraProvider>
+            <Container maxW="1200px" py="10" centerContent>
+                <Router>
+                    <Routes>
+                        <Route index element={<Homepage />} />
+                    </Routes>
+                </Router>
+            </Container>
+        </ChakraProvider>
+    );
 }
 
 export default App;
