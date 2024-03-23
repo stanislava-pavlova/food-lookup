c7b61d9e33aa7fd42a2340e958534be96d29dfbd
Stanislava Pavlova <stanislavap2002@gmail.com>
2024-03-23 22:24:20 +0200

    Update Alert component to include margin top for better spacing

--- frontend/src/components/alert/index.jsx
+++ frontend/src/components/alert/index.jsx
@@ -2,7 +2,7 @@ import { AlertIcon, Alert as ChakraAlert } from '@chakra-ui/react';
 
 const Alert = ({ status, message }) => {
     return (
-        <ChakraAlert status={status}>
+        <ChakraAlert status={status} mt={4}>
             <AlertIcon />
             {message}
         </ChakraAlert>
