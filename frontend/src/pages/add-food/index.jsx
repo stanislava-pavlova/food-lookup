898de3b6dd96a7457e8266e59d3293ff18c5fe8c
Stanislava Pavlova <stanislavap2002@gmail.com>
2024-03-24 11:01:09 +0200

    Updated form submission to post data to the database

--- frontend/src/pages/add-food/index.jsx
+++ frontend/src/pages/add-food/index.jsx
@@ -1,13 +1,54 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Box, Button, FormControl, FormLabel, Heading, Input } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Heading, Input, Spinner } from '@chakra-ui/react';
+import Alert from '../../components/alert';
 
 const AddFood = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => console.log(data);
+    const { register, handleSubmit, reset } = useForm();
+    const [isLoading, setIsLoading] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertType, setAlertType] = useState('');
+
+    const showAlert = (type, message) => {
+        setAlertType(type);
+        setAlertMessage(message);
+        setTimeout(() => {
+            setAlertMessage('');
+        }, 3000);
+    };
+
+    const onSubmit = async (data) => {
+        setIsLoading(true);
+        axios
+            .post('http://localhost:5000/api/foods', data)
+            .then(function (response) {
+                showAlert('success', 'Food added successfully');
+                reset();
+            })
+            .catch(function (error) {
+                showAlert('error', 'Oops, something went wrong.');
+                console.error(error);
+            })
+            .finally(setIsLoading(false));
+    };
+
+    if (isLoading) {
+        return (
+            <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="blue.500"
+                size="xl"
+            />
+        );
+    }
 
     return (
         <Box width="50%">
             <Heading textAlign="center">Add Food Item</Heading>
+            {alertMessage && <Alert status={alertType} message={alertMessage} />}
             <form onSubmit={handleSubmit(onSubmit)}>
                 <FormControl isRequired mt={4}>
                     <FormLabel>Description</FormLabel>
@@ -35,7 +76,7 @@ const AddFood = () => {
                     <Input type="number" step="any" min={0} {...register('carbs')} />
                 </FormControl>
                 <Box display="flex" justifyContent="center" mt={4}>
-                    <Button type="submit" colorScheme="teal">
+                    <Button type="submit" colorScheme="teal" disabled={isLoading}>
                         Add
                     </Button>
                 </Box>
